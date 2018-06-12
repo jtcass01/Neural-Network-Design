23c79de0dd401075f2c6fd5c852077d9c1f18ecd
Jacob T Cassady <jtcass01@louisville.edu>
2018-06-12 16:24:55 -0400

    Finish of Chapter 3 exercise 1.  Some minor upgrades to the implementation of the Hamming and Hopfield Networks.  Implementation of the compet transfer function as I now have a better understanding of its use.

--- Chapter 3 - An Illustrative Example/Exercises/Perceptron.py
+++ Chapter 3 - An Illustrative Example/Exercises/Perceptron.py
@@ -6,12 +6,9 @@ class Perceptron(object):
     """
     Implementation of network described on pages 3-3:3-8
 
-    An output of 1 means the input p is orthogonal of the decision boundary and points in the same direction as the weight.
-    An oupout of -1 means the input p points in the opposite direction of the weight.
-
     Author: Jacob Taylor Cassady
     """
-    def __init__(self, W, b, transfer_function):
+    def __init__(self, W, b, transfer_function=hardlims):
         self.Weights = W
         self.bias = b
         self.transfer_function = transfer_function
@@ -26,9 +23,10 @@ if __name__ == "__main__":
 
     # Weight matrix and bias determined by decision boundary.
     decision_boundary = (orange_prototype != apple_prototype).astype(np.int).reshape((1, len(orange_prototype)))
+    print(decision_boundary)
     bias = 0
 
-    fruit_perceptron = Perceptron(W=decision_boundary, b=bias, transfer_function=hardlims)
+    fruit_perceptron = Perceptron(W=decision_boundary, b=bias)
 
     test_prototype = np.array([-1, -1, -1]).reshape((3, 1))
     print(fruit_perceptron.classify(test_prototype))
---
+++ Chapter 3 - An Illustrative Example/Exercises/e3_1.py
@@ -0,0 +1,52 @@
+
+"""
+In this chapter we have designed three different neural networks to distinguish between apples and oranges, based on three sensor mesurements (shape, texture, and weight).
+Suppose that we want to distinguish between bananas and pineapples:
+bananas    = [-1, 1, -1].T
+Pineapples = [-1, -1, 1].T
+
+i.   Design a perceptron to recognize these patterns.
+ii.  Design a Hamming network to recognize these patterns.
+iii. Design a Hopfield network to recognize thse patterns.
+iv.  Test the operation of your networks by applying several different input patterns.  Discuss the advantages and disadvantages of each network.
+"""
+
+import numpy as np
+
+from Perceptron import Perceptron
+from HammingNetwork import HammingNetwork
+from HopfieldNetwork import HopfieldNetwork
+
+banana_prototype = np.array([-1, 1, -1]).reshape((3, 1))
+pineapple_prototype = np.array([-1, -1, 1]).reshape((3, 1))
+prototypes = np.array([banana_prototype.T[0], pineapple_prototype.T[0]])
+
+near_pineapple = np.array([1, -1, 1]).reshape((3, 1))
+near_banana = np.array([-1, 1, -1]).reshape((3, 1))
+
+if __name__ == "__main__":
+    print(prototypes)
+
+    print("\ni.   Design a perceptron to recognize these patterns.")
+    # Decision boundary is a weight that points towards that of a banana.
+    print("\tNote: Decision boundary is a weight that points towards that of a banana.")
+    decision_boundary = np.array([0, 1, -1])
+    
+    bp_perceptron = Perceptron(W=decision_boundary, b=0)
+    print("\t", "near_pineapple", bp_perceptron.classify(near_pineapple))
+    print("\t", "near_banana", bp_perceptron.classify(near_banana))
+
+    print("\nii.  Design a Hamming network to recognize these patterns.")
+    bp_hamming = HammingNetwork(prototypes=prototypes)
+    print("\t", "near_pineapple", bp_hamming.classify(near_pineapple))
+    print("\t", "near_banana", bp_hamming.classify(near_banana))
+
+    print("\niii. Design a Hopfield network to recognize thse patterns.")
+    bp_hopfield = HopfieldNetwork(W = np.array([[0.2, 0, 0], [0, 1.2, 0], [0, 0, 1.2]]), bias=np.array([-0.9, 0, 0]))    
+    print("\t", "near_pineapple", bp_hopfield.classify(near_pineapple))
+    print("\t", "near_banana", bp_hopfield.classify(near_banana))
+
+    print("\niv.  Test the operation of your networks by applying several different input patterns.  Discuss the advantages and disadvantages of each network.")
+    print("I am going to skip the test of different input patterns as the two prototypes are different in 2/3s of the their presentations.  I fear any tests my sully my understanding as I am not sure which result I should be getting to verify.",
+          "The hamming network seems to have the simplest implementation as it's weights and biases are defined by the prototypes themselves without the use of much math to calculate complex things such as the eigen matrix in the Hopfield Network",
+          "Or the decision boundary within the perceptron network.")
