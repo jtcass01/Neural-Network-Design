5315973060a7c82a5c1db8de4fe18e68d0ef480b
Jacob T Cassady <jtcass01@louisville.edu>
2018-06-21 08:31:04 -0400

    Implemented unified learning rule for perceptrons

--- Chapter 4 - Perceptron Learning Rule/Examples/Perceptron.py
+++ Chapter 4 - Perceptron Learning Rule/Examples/Perceptron.py
@@ -11,27 +11,76 @@ class Perceptron(object):
 
     Author: Jacob Taylor Cassady
     """
-    def __init__(self, W, b, transfer_function=hardlims):
-        self.Weights = W
-        self.bias = b
+    def __init__(self, number_of_neurons, input_size, transfer_function=hardlim):
+        self.Weights = np.random.rand(number_of_neurons, input_size)
+        self.bias = np.random.rand(number_of_neurons,1)
         self.transfer_function = np.vectorize(transfer_function)
+
     def classify(self, prototype):
         net_input = self.Weights.dot(prototype) + self.bias
         return self.transfer_function(net_input)
 
+    def train(self, prototypes):
+        old_weights = self.Weights
+        old_bias = self.bias
+
+        while not self.correct(prototypes):
+            for prototype in prototypes:
+                input_v = prototype[0]
+                target = prototype[1]
+
+                classification = self.classify(prototype = input_v)
+
+                self.Weights = self.Weights + (target-classification)*input_v.T
+                self.bias = self.bias + (target-classification)
+
+    def correct(self, prototypes):
+        for prototype in prototypes:
+            input_v = prototype[0]
+            target = prototype[1]
+
+            if target != self.classify(input_v):
+                return False
+        return True
 
 if __name__ == "__main__":
-    # prototype = [shape, texture, weight] as a column vector
-    orange_prototype = np.array([1, -1, -1]).reshape((3, 1))
-    apple_prototype = np.array([1, 1, -1]).reshape((3, 1))
+    p_1 = np.array([1, -1, -1]).reshape((3,1))
+    t_1 = np.array([0]).reshape((1,1))
 
-    # Weight matrix and bias determined by decision boundary.
-    decision_boundary = (orange_prototype != apple_prototype).astype(np.int).reshape((1, len(orange_prototype)))
-    print(decision_boundary)
-    bias = 0
+    p_t_1 = np.array([p_1, t_1])
 
-    fruit_perceptron = Perceptron(W=decision_boundary, b=bias)
+    p_2 = np.array([1, 1, -1]).reshape((3,1))
+    t_2 = np.array([1]).reshape((1,1))
 
-    test_prototype = np.array([-1, -1, -1]).reshape((3, 1))
-    print(fruit_perceptron.classify(test_prototype))
+    p_t_2 = np.array([p_2, t_2])
 
+    prototypes = np.array([p_t_1, p_t_2])
+
+    print(prototypes[0][0].shape[0])
+
+    test_perceptron = Perceptron(number_of_neurons=1, input_size=prototypes[0][0].shape[0])
+    test_perceptron.train(prototypes=prototypes)
+    print("Train complete", test_perceptron.Weights, test_perceptron.bias)
+
+
+    p_1 = np.array([0, 0]).reshape((2,1))
+    t_1 = np.array([0]).reshape((1,1))
+    p_t_1 = np.array([p_1, t_1])
+
+    p_2 = np.array([0, 1]).reshape((2,1))
+    t_2 = np.array([0]).reshape((1,1))
+    p_t_2 = np.array([p_2, t_2])
+
+    p_3 = np.array([1, 0]).reshape((2,1))
+    t_3 = np.array([0]).reshape((1,1))
+    p_t_3 = np.array([p_3, t_3])
+
+    p_4 = np.array([1, 1]).reshape((2,1))
+    t_4 = np.array([1]).reshape((1,1))
+    p_t_4 = np.array([p_4, t_4])
+
+    prototypes = np.array([p_t_1, p_t_2, p_t_3, p_t_4])
+
+    test_perceptron = Perceptron(number_of_neurons=1, input_size=prototypes[0][0].shape[0])
+    test_perceptron.train(prototypes=prototypes)
+    print("Train complete", test_perceptron.Weights, test_perceptron.bias)
--- Chapter 4 - Perceptron Learning Rule/Examples/decision_boundary.py
+++ Chapter 4 - Perceptron Learning Rule/Examples/decision_boundary.py
@@ -1,7 +1,46 @@
 
 """
-AND GATE implementation, find the decision boundary, weights, and bias.
+AND GATE perceptron implementation, find the decision boundary, weights, and bias.
 
 Use hardlim activation
 """
 
+import numpy as np
+import matplotlib.pyplot as plt
+
+from transfer_functions import hardlim
+from Perceptron import Perceptron
+
+# target = 0
+and_00 = np.array([0, 0]).reshape((2, 1))
+
+# target = 0
+and_10 = np.array([1, 0]).reshape((2, 1))
+
+# target = 0
+and_01 = np.array([0, 1]).reshape((2, 1))
+
+# target = 1
+and_11 = np.array([1, 1]).reshape((2, 1))
+
+and_prototypes = np.array([and_00.T[0], and_10.T[0], and_01.T[0], and_11.T[0]])
+
+if __name__ == "__main__":
+    print(and_prototypes)
+
+    weights = np.array([1, 1]).reshape((1, 2))
+    bias = -1.5
+
+    x = np.linspace(0, 2, 50)
+    y = x
+
+    perceptron = Perceptron(W = weights, b = bias, transfer_function = hardlim)
+
+    for prototype in and_prototypes:
+        print("prototype", prototype)
+        print("Classification: ", perceptron.classify(prototype=prototype.T))
+
+    plt.scatter(and_prototypes[:, 0], and_prototypes[:, 1])
+    plt.plot(x,y, color='black')
+    plt.grid(True)
+    plt.show()
